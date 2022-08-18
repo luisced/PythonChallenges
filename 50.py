842d290fd8dd0872cc1153c47db167b263dbc196
Luis <luisced02031@gmail.com>
2022-08-17 22:07:51 -0500

    Sum of fifty and maxnumber

--- 50.py
+++ 50.py
@@ -1,4 +1,3 @@
-import enum
 
 
 list1 = [0, 1, 2, 10, 4, 5, 6, 7, 8, 9]
@@ -6,14 +5,21 @@ max_number = list1[0]
 for i in list1:
     if i > max_number:
         max_number = i
+    else:
+        continue
 
 print(max_number)
 
+# explain this code
+
 
 x = (50 *(50 + 1))/2
+promedio = x/50
 
+print(f"El resultado es {(50 *(50 + 1))/2 }")
+print(f"El promedio es {((50 *(50 + 1))/2)/50}")
 
 n = 0
 for i in range(51):
     n += i
-print(f"El resultado es: {n}")
+print(f"El resultado es: {n} y el promedio es: {n/50}")
